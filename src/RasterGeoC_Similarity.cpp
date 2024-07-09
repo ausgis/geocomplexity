97973347fae84dce562de05fe4100c1163136fcb
SpatLyu <lyu.geosocial@gmail.com>
2024-07-09 12:17:54 +0800

    try to support gcs in geocs_raster

--- src/RasterGeoC_Similarity.cpp
+++ src/RasterGeoC_Similarity.cpp
@@ -8,31 +8,49 @@ using namespace Rcpp;
 NumericVector RasterGeoCSimilarity(NumericMatrix x,
                                    IntegerMatrix iw,
                                    int w, int method){
-  NumericVector out(x.nrow());
-  IntegerVector w_sp = extract_window(iw,w);
-  int ncell = pow(w,2);
-  for (int n = 0; n < x.nrow(); ++n){
-    IntegerVector wi = rcpp_seq(n*ncell,(n+1)*ncell-1);
-    // Rcout << "wi_1: " << wi << "\n";
-    wi = w_sp[wi];
-    // Rcout << "wi_2: " << wi << "\n";
-    wi = wi[!is_na(wi)];
-    // Rcout << "wi_3: " << wi << "\n";
-    // Rcout << "wi_3.size(): " << wi.size() << "\n";
-    NumericVector zn = x(n,_);
-    NumericVector resn(wi.size());
-    for (int i = 0 ; i < wi.size(); ++i){
-      NumericVector zi = x(wi[i],_);
-      double zs = CosineSimilarity(zn,zi);
-      resn[i] = zs;
-    }
-    double res = 0;
-    for(int i = 0; i < wi.size(); ++i) {
-      for(int j = 0; j < wi.size(); ++j) {
-        res += pow((resn[i]-resn[j]),2) / 2;
+  if (method != 1){
+    NumericVector out(x.nrow());
+    IntegerVector w_sp = extract_window(iw,w);
+    int ncell = pow(w,2);
+    for (int n = 0; n < x.nrow(); ++n){
+      IntegerVector wi = rcpp_seq(n*ncell,(n+1)*ncell-1);
+      // Rcout << "wi_1: " << wi << "\n";
+      wi = w_sp[wi];
+      // Rcout << "wi_2: " << wi << "\n";
+      wi = wi[!is_na(wi)];
+      // Rcout << "wi_3: " << wi << "\n";
+      // Rcout << "wi_3.size(): " << wi.size() << "\n";
+      NumericVector zn = x(n,_);
+      NumericVector resn(wi.size());
+      for (int i = 0 ; i < wi.size(); ++i){
+        NumericVector zi = x(wi[i],_);
+        double zs = CosineSimilarity(zn,zi);
+        resn[i] = zs;
       }
+      double res = 0;
+      for(int i = 0; i < wi.size(); ++i) {
+        for(int j = 0; j < wi.size(); ++j) {
+          res += pow((resn[i]-resn[j]),2) / 2;
+        }
+      }
+      out[n] = res / pow(wi.size(),2);
     }
-    out[n] = res / pow(wi.size(),2);
+    return out;
+  } else {
+    NumericMatrix wt (x.nrow(),x.nrow());
+    IntegerVector w_sp = extract_window(iw,w);
+    int ncell = pow(w,2);
+    for (int n = 0; n < x.nrow(); ++n){
+      IntegerVector wi = rcpp_seq(n*ncell,(n+1)*ncell-1);
+      wi = w_sp[wi];
+      // Rcout << "wi_2: " << wi << "\n";
+      wi[is_na(wi)] = 0;
+      // Rcout << "wi_3: " << wi << "\n";
+      // Rcout << "wi_3.size(): " << wi.size() << "\n";
+      wt(n,_) = wi;
+    }
+
+    NumericVector out = GCS_Variance(x,wt);
+    return out;
   }
-  return out;
 }
