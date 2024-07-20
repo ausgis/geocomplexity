487fb34e4508d7d57673ecfcfa15c8425d3eb420
SpatLyu <lyu.geosocial@gmail.com>
2024-07-20 11:08:46 +0800

    update geocd_* document

--- R/geocd_raster.R
+++ R/geocd_raster.R
@@ -19,11 +19,11 @@
 #' @param normalize (optional) Whether to further normalizes the calculated geocomplexity.
 #' Default is `TRUE`.
 #' @param method (optional) In instances where the method is `moran`, geocomplexity is
-#' determined using LISA. Conversely, when the method is `spvar`, the spatial variance
-#' of attribute data serves to characterize geocomplexity. For all other methods, the
-#' shannon information entropy of attribute data is employed to represent geocomplexity.
-#' The selection of the method can be made from any one of the three options: `moran`,
-#' `spvar` or `entropy`. Default is `moran`.
+#' determined using local moran measure method. Conversely, when the method is `spvar`,
+#' the spatial variance of attribute data serves to characterize geocomplexity. For all
+#' other methods, the shannon information entropy of attribute data is employed to represent
+#' geocomplexity. The selection of the method can be made from any one of the three options:
+#' `moran`, `spvar` or `entropy`. Default is `moran`.
 #'
 #' @return A SpatRaster object
 #' @export
