48119bc684487aa6198f7077a5e3136cb5bc8e7b
SpatLyu <lyu.geosocial@gmail.com>
2024-08-12 01:02:53 +0800

    export magrittr pipe

--- R/pipe.R
+++ R/pipe.R
@@ -1,5 +1,11 @@
-#' Pipe operator
+#' @title Pipe operator
+#' @description
+#' See \code{magrittr::\link[magrittr:pipe]{\%>\%}} for details.
+#'
+#' @name %>%
+#' @usage lhs \%>\% rhs
+#' @rdname pipe
+#' @keywords internal
 #' @importFrom magrittr %>%
 #' @export
-#' @noRd
 NULL
