4d839ad435426b8aa1eee267d7e97a69f935b3e8
SpatLyu <lyu.geosocial@gmail.com>
2024-08-26 17:31:43 +0800

    add geoc_swt document

--- R/geoc_swt.R
+++ R/geoc_swt.R
@@ -1,3 +1,21 @@
+#' @title spatial weight matrix based on geographical complexity
+#'
+#' @param gc Geographical complexity vector.
+#' @param wt Spatial weight matrix based on spatial adjacency or spatial distance relationships.
+#' @param style (optional) A character that can be `B`,`W`,`C`.  More to see `spdep::nb2mat()`.
+#' Default is `B`.
+#'
+#' @return A matrix
+#' @export
+#'
+#' @examples
+#' data("income")
+#' inc = dplyr::select(income,Income)
+#' gc = geocd_vector(inc,returnsf = FALSE)
+#' wt = sdsfun::spdep_contiguity_swm(inc,style = 'B')
+#' wt_gc = geoc_swt(gc,wt)
+#' wt_gc[1:5,1:5]
+#'
 geoc_swt = \(gc,wt,style = 'B'){
   wt_gc = GeoCSWT(gc,wt,style)
   return(wt_gc)
