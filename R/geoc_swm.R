e5652cfce6b01616d5e36562cfb12f533ba12c0f
SpatLyu <lyu.geosocial@gmail.com>
2024-08-29 14:51:33 +0800

    update geoc_swm

--- R/geoc_swm.R
+++ R/geoc_swm.R
@@ -3,7 +3,7 @@
 #' @param gc Geographical complexity vector.
 #' @param wt Spatial weight matrix based on spatial adjacency or spatial distance relationships.
 #' @param style (optional) A character that can be `B`,`W`,`C`.  More to see `spdep::nb2mat()`.
-#' Default is `B`.
+#' Default is `W`.
 #'
 #' @return A matrix
 #' @export
@@ -18,7 +18,7 @@
 #' wt_gc = geoc_swm(gc,wt)
 #' wt_gc[1:5,1:5]
 #'
-geoc_swm = \(gc,wt,style = 'B'){
+geoc_swm = \(gc,wt,style = 'W'){
   wt_gc = GeoCSWT(gc,wt,style)
   return(wt_gc)
 }
