673b25e635e7981868686ed517160a17bb29b374
SpatLyu <lyu.geosocial@gmail.com>
2024-09-11 23:11:38 +0800

    rm internal data

--- R/data.R
+++ R/data.R
@@ -1,10 +1,1 @@
-#' @title Spatial datasets of income.
-#'
-#' @description Spatial datasets of income.
-#'
-#' @name income
-#' @format \code{income}: An sf object about income
-#' @docType data
-#' @author Yongze Song \email{yongze.song@curtin.edu.au}
-#' @keywords dataset income
-"income"
+
