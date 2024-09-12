b7c0e3ee421c2ba5329e4be96e07067dae6af063
SpatLyu <lyu.geosocial@gmail.com>
2024-09-13 02:24:02 +0800

    update AICc calculation

--- src/GeoC_GWR.cpp
+++ src/GeoC_GWR.cpp
@@ -23,6 +23,16 @@ arma::vec Normalize4Interval(const arma::vec& v, double a, double b) {
   return v_scaled;
 }
 
+arma::mat DiagMatrix(int n) {
+  // Create a vector of ones with length n
+  arma::vec diag_elements = arma::ones<arma::vec>(n);
+
+  // Create a diagonal matrix using the vector
+  arma::mat diag_mat = arma::diagmat(diag_elements);
+
+  return diag_mat;
+}
+
 // [[Rcpp::export]]
 Rcpp::List GeoCGWRFit(arma::vec y, arma::mat X,
                       arma::vec gcs, arma::mat Cdist,
@@ -89,14 +99,28 @@ Rcpp::List GeoCGWRFit(arma::vec y, arma::mat X,
     se_betas.row(i) = se_beta_i.t();
   }
 
+  // refer to https://github.com/rsbivand/spgwr/blob/main/R/gwr.R
   arma::mat t_values = betas / se_betas;
   double rss = arma::as_scalar(arma::sum(arma::pow(residuals, 2)));
   double tss = arma::as_scalar(arma::sum(arma::pow(y - arma::mean(y), 2)));
   double r2 = 1 - (rss / tss);
   double adjr2 = 1 - (1 - r2) * (n - 1) / (n - k - 1);
   double rmse = sqrt(rss / n);
-  double aic = n * log(rss / n) + 2 * k;
-  double aicc = aic + (2 * k * (k + 1)) / (n - k - 1);
+  double v1 = arma::trace(hat_matrix);
+  arma::mat B2 = hat_matrix.t() * hat_matrix;
+  double v2 = arma::trace(B2);
+  // effective d.f. is n - 2*v1 + v2
+  double edf = n - 2*v1 + v2;
+  arma::mat B1 = (DiagMatrix(n) - hat_matrix).t() * (DiagMatrix(n) - hat_matrix);
+  double delta1 = arma::trace(B1);
+  double sigma2 = rss / delta1;
+  double odelta2 = sum(arma::pow(arma::diagvec(B1), 2));
+  double delta2 = sum(arma::diagvec(B1*B1));
+  double nu1 = arma::trace(B2);
+  double sigma2_b = rss / n;
+  double aicbb = 2*n*log(sqrt(sigma2_b)) + n*log(2*datum::pi) + (n * ((n + v1) / (n - 2 - v1)));
+  double aichb = 2*n*log(sqrt(sigma2_b)) + n*log(2*datum::pi) + n + v1;
+  double aiccb = 2*n*log(sqrt(sigma2_b)) + n*log(2*datum::pi) + n * ((delta1/delta2)*(n + nu1))/((pow(delta1,2)/delta2)-2);
 
   return Rcpp::List::create(
     Named("Coefficient") = betas,
@@ -105,10 +129,12 @@ Rcpp::List GeoCGWRFit(arma::vec y, arma::mat X,
     Named("yhat") = yhat,
     Named("Residuals") = residuals,
     Named("RSS") = rss,
+    Named("EDF") = edf,
     Named("R2") = r2,
     Named("R2_Adj") = adjr2,
     Named("RMSE") = rmse,
-    Named("AIC") = aic,
-    Named("AICc") = aicc
+    Named("AICb") = aicbb,
+    Named("AICh") = aichb,
+    Named("AICc") = aiccb
   );
 }
