ac0d981438b96d6f082344c1626308dbdcc64510
SpatLyu <lyu.geosocial@gmail.com>
2024-09-20 12:57:15 +0800

    add geocd_swm function

--- R/geoc_swm.R
+++ R/geoc_swm.R
@@ -24,3 +24,35 @@ geocs_swm = \(sfj, wt = NULL, style = 'B', ...){
 
   return(wt_gc)
 }
+
+#' @title constructing spatial weight matrix based on geocomplexity with spatial dependence
+#'
+#' @param sfj An `sf` object or spatial vector object that can be converted to `sf` by `sf::st_as_sf()`.
+#' @param wt (optional) Spatial weight matrix based on spatial adjacency or spatial distance relationships.
+#' @param style (optional) A character that can be `B`,`W`,`C`.  More to see `spdep::nb2mat()`.
+#' Default is `B`.
+#' @param ... (optional) Other parameters passed to `geocomplexity::geocd_vector()`.
+#'
+#' @return A matrix
+#' @export
+#'
+#' @examples
+#' econineq = sf::read_sf(system.file('extdata/econineq.gpkg',package = 'geocomplexity'))
+#' wt_gc = geocd_swm(econineq)
+#' wt_gc[1:5,1:5]
+#'
+geocd_swm = \(sfj, wt = NULL, style = 'B', ...){
+  if (is.null(wt)) {
+    wt = sdsfun::spdep_contiguity_swm(sfj,
+                                      queen = TRUE,
+                                      style = 'B',
+                                      zero.policy = TRUE)
+  }
+
+  gc = geocomplexity::geocd_vector(sfj,wt,...) %>%
+    sf::st_drop_geometry() %>%
+    as.matrix()
+  wt_gc = GeoCD_SWM(gc,wt,style)
+
+  return(wt_gc)
+}
